bf263c06f634c7f75152e0172f89fe95adf83009
hechmi10 <157498918+hechmi10@users.noreply.github.com>
2024-02-14 18:04:12 +0100

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<style>
+    body{
+        background-image:url("https://cdn.pixabay.com/photo/2015/02/21/17/11/hay-bales-644440_640.jpg");
+        background-repeat: no-repeat;
+        background-size:cover;
+        font-size:25px;
+        color:green;
+        font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', 'Arial', 'sans-serif';
+    }
+</style>
+<body>
+    <center>
+        <h1>
+            Numéros de telephone:<br>
+            11223344<br>
+            55667788<br>
+            99001122<br>
+            33445566<br>
+            77889900<br>
+        </h1>
+    </center>
+</body>
+</html>
