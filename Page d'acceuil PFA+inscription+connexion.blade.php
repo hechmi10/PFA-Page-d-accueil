9bd160db8b168efc621e8dbfb3bb14a023f7a6c1
hechmi10 <157498918+hechmi10@users.noreply.github.com>
2024-02-17 20:51:36 +0100

    Update Page d'acceuil PFA+inscription+connexion.blade.php

--- Page d'acceuil PFA+inscription+connexion.blade.php
+++ Page d'acceuil PFA+inscription+connexion.blade.php
@@ -1,7 +1,7 @@
 <?php
 function connexionBD(){
     try{
-        $pdo=new PDO("mysql:host=localhost;dbname=agri_connect","root","");
+        $pdo=new PDO('mysql:host=localhost;dbname=agri_connect','root','');
         return $pdo;
     }
     catch(Exception $e){
@@ -14,15 +14,14 @@ function deconnexionBD(){
 }
 $c=connexionBD();
 if(isset($_POST['inscrire'])){
+    $cin=isset($_POST['cin']) ? $_POST['cin'] : '';
     $nom=isset($_POST['nom']) ? $_POST['nom'] : '';
     $prenom=isset($_POST['prenom']) ? $_POST['prenom'] : '';
-    $cin=isset($_POST['cin']) ? $_POST['cin'] : '';
     $mdp=isset($_POST['mdp']) ? $_POST['mdp'] : '';
     $email=isset($_POST['email']) ? $_POST['email'] : '';
     $numtel=isset($_POST['numtel']) ? $_POST['numtel'] : '';
-    $inscrire=isset($_POST['inscrire']) ? 1 : 0;
-    if(!(empty($nom)) && (!(empty($prenom))) && (!(empty($cin))) && (!(empty($mdp))) && (!(empty($email))) && (!(empty($numtel))) && (!(empty($inscrire)))){
-        $stat1=$c->prepare("INSERT INTO agriculteurs (Cin,Nom,Prènom,motDePasse,Email,NumTel) VALUES ('$cin','$nom','$prenom','$mdp','$email','$numtel');");
+    if((!empty($nom)) && (!empty($prenom)) && (!empty($cin)) && (!empty($mdp)) && (!empty($email)) && (!empty($numtel))){
+        $stat1=$c->query("INSERT INTO agriculteurs (Cin,Nom,Prènom,MotDePasse,Email,NumTel) VALUES ('$cin','$nom','$prenom','$mdp','$email','$numtel');");
         if($stat1->execute()){
             echo "Inscription réussie";
         }
@@ -30,16 +29,22 @@ if(isset($_POST['inscrire'])){
             echo "Inscription échouée,veuillez réessayer";
         }
     }
-    if(!empty($email) && !empty($mdp) && !empty($inscrire)){
-        $stat2=$c->prepare("SELECT * FROM agriculteurs;");
-        while($r=$stat2->fetch(PDO::FETCH_NUM)){
-            foreach($r as $v){
-                echo "$v"."<br>";
+}
+if(isset($_POST['connecter'])){
+    $email=isset($_POST['email']) ? $_POST['email'] : '';
+    $mdp=isset($_POST['mdp']) ? $_POST['mdp'] : '';
+    if((!empty($email)) && (!empty($mdp))){
+        $stat2=$c->query("SELECT * FROM agriculteurs;");
+        if($stat2->execute()){
+            while($r=$stat2->fetch(PDO::FETCH_NUM)){
+                foreach($r as $v){
+                    echo "$v"."<br>";
+                }
             }
         }
-    }
-    else{
-        echo "Connexion échouée,veuillez réessayer";
+        else{
+            echo "Connexion échouée,veuillez réessayer";
+        }
     }
 }
 $d=deconnexionBD();
