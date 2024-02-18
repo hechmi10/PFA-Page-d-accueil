9235721a3a105a4d21f927dad1e59d686a6b0ede
hechmi10 <157498918+hechmi10@users.noreply.github.com>
2024-02-18 21:11:56 +0100

    Update inscriptionAgriculteurbackend.blade.php

--- inscriptionAgriculteurbackend.blade.php
+++ inscriptionAgriculteurbackend.blade.php
@@ -1,6 +1,7 @@
 <?php
 require("Page d'acceuil PFA Connexion BD.blade.php");
-if((isset($_POST['cin'])) && (isset($_POST['nom'])) && (isset($_POST['prenom'])) && (isset($_POST['mdp'])) && (isset($_POST['email'])) && (isset($_POST['numtel'])) && (isset($_POST['inscrire']))){
+$c=connexionBD();
+if((isset($_POST['cin'])) && (isset($_POST['nom'])) && (isset($_POST['prenom'])) && (isset($_POST['mdp'])) && (isset($_POST['email'])) && (isset($_POST['numtel']))){
     $cin=isset($_POST['cin']) ? $_POST['cin'] : '';
     $nom=isset($_POST['nom']) ? $_POST['nom'] : '';
     $prenom=isset($_POST['prenom']) ? $_POST['prenom'] : '';
@@ -8,18 +9,21 @@ if((isset($_POST['cin'])) && (isset($_POST['nom'])) && (isset($_POST['prenom']))
     $email=isset($_POST['email']) ? $_POST['email'] : '';
     $numtel=isset($_POST['numtel']) ? $_POST['numtel'] : '';
     if((!empty($nom)) && (!empty($prenom)) && (!empty($cin)) && (!empty($mdp)) && (!empty($email)) && (!empty($numtel))){
-        $c=connexionBD();
-        $stat1=$c->query("INSERT INTO agriculteurs (Cin,Nom,Prènom,MotDePasse,Email,NumTel) VALUES ('$cin','$nom','$prenom','$mdp','$email','$numtel');");
+        $stat1=$c->prepare("INSERT INTO agriculteurs (Cin,Nom,Prènom,MotDePasse,Email,NumTel) VALUES ('$cin','$nom','$prenom','$mdp','$email','$numtel')");
         if($stat1->execute()){
             echo "Inscription réussie";
-            $d=deconnexionBD();
         }
         else{
             echo "Inscription échouée,veuillez réessayer";
         }
+        $d=deconnexionBD();
+    }
+    else{
+        echo "les champs sont obligatoires!!";
     }
 }
 else{
     echo "non valide";
 }
+$d=deconnexionBD();
 ?>
