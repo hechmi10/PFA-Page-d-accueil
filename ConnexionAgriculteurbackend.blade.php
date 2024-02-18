0d727c86f7202344b4828e57a842950a17cce9d8
hechmi10 <157498918+hechmi10@users.noreply.github.com>
2024-02-18 22:09:15 +0100

    Update ConnexionAgriculteurbackend.blade.php

--- ConnexionAgriculteurbackend.blade.php
+++ ConnexionAgriculteurbackend.blade.php
@@ -1,24 +1,23 @@
 <?php
 require("Page d'acceuil PFA connexion BD.blade.php");
 $c = connexionBD();
-if(isset($_POST['email'], $_POST['mdp'])){
+if((isset($_POST['email'])) && (isset($_POST['mdp']))){
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $mdp = isset($_POST['mdp']) ? $_POST['mdp'] : '';
     if(!empty($email) && !empty($mdp)){
-        $stat2 = $c->prepare("SELECT * FROM agriculteurs WHERE Email = :email AND MotDePasse = :mdp");
+        $stat2 = $c->prepare("SELECT * FROM agriculteurs WHERE Email = '$email' AND MotDePasse = '$mdp'");
         $stat2->execute(array('email' => $email, 'mdp' => $mdp));
         $result = $stat2->fetchAll(PDO::FETCH_ASSOC);
-
-        if($result){
-            foreach($result as $row){
-                foreach($row as $key => $value){
-                    echo "$key: $value <br>";
-                }
+        while($result){
+            foreach($row as $key => $value){
+                echo "$key: $value" . "<br>";
             }
-        } else {
-            echo "Aucun agriculteur trouvé avec ces identifiants.";
-        }
+        } 
     }
+    else {
+        echo "Aucun agriculteur trouvé avec ces identifiants.";
+    }
+    
 }
 else{
     echo "Des données sont manquantes.";
