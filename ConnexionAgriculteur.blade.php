f91ef7a287f34db37268ffeced88107998969fae
hechmi10 <157498918+hechmi10@users.noreply.github.com>
2024-03-24 19:34:11 +0100

    Update ConnexionAgriculteur.blade.php

--- ConnexionAgriculteur.blade.php
+++ ConnexionAgriculteur.blade.php
@@ -1,77 +1,259 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Connexion Agriculteur</title>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>AgriConnect</title>
+
+    <!-- slider stylesheet -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="bootstrap.css" />
+    <!-- fonts awesome style -->
+    <link href="font-awesome.min.css" rel="stylesheet" />
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,,600,700|Poppins:400,500,700&display=swap"
+        rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="responsive.css" rel="stylesheet" />
 </head>
-<style>
-body {
-  background: #E3F2FD;
-  background-image:url("https://cdn.pixabay.com/photo/2014/09/09/19/07/corn-field-440338_640.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-.wrapper{
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border-radius:10%;
-  height: fit-content;
-  width:max-content;
-  background-position: center;
-  font-size: medium;
-  font-family: 'Segoe UI', Tahoma, 'Geneva', Verdana, sans-serif;
-  font-style: normal;
-}
-input[type="text"]{
-    height:fit-content;
-    width: fit-content;
-    border-radius: 50px;
-    background-color: greenyellow;
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva', Verdana, sans-serif;
-    font-size: 25px;
-}
-input[type="password"],input[type="email"]{
-    height:fit-content;
-    width: fit-content;
-    border-radius: 50px;
-    background-color: greenyellow;
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva', Verdana, sans-serif;
-    font-size: 25px;
-}
-input[type="submit"]{
-  background-color:white;
-  height:1cm;
-  width:5cm;
-  border-radius:20%;
-}
-fieldset{
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border-radius:10%;
-  height: fit-content;
-  width:max-content;
-  background-color: green;
-  border-color: greenyellow;
-}
-</style>
-<body>
-  <form action="ConnexionAgriculteurbackend.blade.php" method="post">
-    <div class="wrapper">
-      <fieldset>
-        <center><h1>Connexion d'un Agriculteur</h1></center>
-        <label>Email:</label><br>
-        <input type="email" name="email" placeholder="Email" id="email" required><br>
-        <label>Mot de passe:</label><br>
-        <input type="password" name="mdp" placeholder="Mot de passe" id="mdp" required><br>
-        <input type="submit" name="connecter" value="Se connecter">
-      </fieldset>
+
+<body class="sub_page">
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <nav class="navbar navbar-expand-lg custom_nav-container">
+                <div class="custom_menu-btn">
+                    <button onclick="openNav()">
+                        <span class="s-1"> </span>
+                        <span class="s-2"> </span>
+                        <span class="s-3"> </span>
+                    </button>
+                </div>
+                <div id="myNav" class="overlay">
+                    <div class="menu_btn-style ">
+                        <button onclick="closeNav()">
+                            <span class="s-1"> </span>
+                            <span class="s-2"> </span>
+                            <span class="s-3"> </span>
+                        </button>
+                    </div>
+                    <div class="overlay-content">
+                        <a class="" href="PagedacceuilPFA.blade.php">
+                            Acceuil
+                        </a>
+                        <a class="" href="marketplace.html">
+                            Marketplace
+                        </a>
+                        <a class="" href="Gestiondesfermes.html">
+                            Gestion des fermes
+                        </a>
+                        <a class="" href="InscriptionAgriculteur.blade.php">
+                            Inscription
+                        </a>
+                        <a class="" href="ConnexionAgriculteur.blade.php">
+                            Connexion
+                        </a>
+                        <a class="" href="jeudesimulation.html">
+                            Jeu de simulation
+                        </a>
+                        <a class="" href="contact.blade.php">
+                            Contact
+                        </a>
+                    </div>
+                </div>
+                <a class="navbar-brand" href="index.html">
+                    <span>
+                        AgriConnect
+                    </span>
+                </a>
+                <div class="user_option">
+                    <form class="form-inline">
+                        <button class="btn  nav_search-btn" type="submit">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                    <a href="">
+                        <i class="fa fa-user" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </nav>
+        </header>
+        <!-- end header section -->
     </div>
-</form>
+
+    <!-- client section -->
+
+    <section class="contact_section layout_padding section_pl ">
+        <div class="container py_mobile_45">
+            <div class="heading_container">
+                <h2><span>Connexion</span></h2>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form_container">
+                        <form action="ConnexionAgriculteurbackend.blade.php" method="post">
+                            <label>Email:</label><br>
+                            <input type="email" name="email" placeholder="Email" id="email" required><br>
+                            <label>Mot de passe:</label><br>
+                            <input type="password" name="mdp" placeholder="Mot de passe" id="mdp"
+                                required><br>
+                            <input type="submit" name="connecter" value="Se connecter">
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="map_container">
+                        <div class="map">
+                            <div id="googleMap"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end client section -->
+
+    <!-- info section -->
+
+    <section class="info_section layout_padding section_pl">
+        <div class="container">
+            <div class="info_logo">
+                <a href="">
+                    AgriConnect
+                </a>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="info_contact">
+                        <h4>
+                            Adresse
+                        </h4>
+                        <div class="contact_link_box">
+                            <a href="">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                <span>
+                                    Location
+                                </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <span>
+                                    Call +01 1234567890
+                                </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                <span>
+                                    Email : demo@gmail.com
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="info_social">
+                        <a href="">
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="info_link_box">
+                        <h4>
+                            Links
+                        </h4>
+                        <div class="info_links">
+                            <a class="" href="PagedacceuilPFA.blade.php">
+                                Acceuil
+                            </a>
+                            <a class="" href="marketplace.html">
+                                Marketplace
+                            </a>
+                            <a class="" href="Gestiondesfermes.html">
+                                Gestion des fermes
+                            </a>
+                            <a class="" href="InscriptionAgriculteur.blade.php">
+                                Inscription
+                            </a>
+                            <a class="" href="ConnexionAgriculteur.blade.php">
+                                Connexion
+                            </a>
+                            <a class="" href="jeudesimulation.html">
+                                Jeu de simulation
+                            </a>
+                            <a class="" href="contact.blade.php">
+                                Contact
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="info_detail">
+                            <h4>
+                                Produits
+                            </h4>
+                            <p>
+                                necessary, making this the first true generator on the Internet. It uses a dictionary of
+                                over 200 Latin words, combined with a handful
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <h4>
+                            Subscribe
+                        </h4>
+                        <form action="">
+                            <input type="text" placeholder="Your Email" />
+                            <button type="submit">
+                                Subscribe
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <!-- end info section -->
+
+    <!-- footer section -->
+    <footer class="footer_section">
+        <div class="container">
+            <p>
+                &copy; <span id="displayYear"></span> All Rights Reserved. Design by
+                <a href="https://html.design/">Free Html Templates</a>
+            </p>
+        </div>
+    </footer>
+    <!-- footer section -->
+
+    <script src="jquery-3.4.1.min.js"></script>
+    <script src="bootstrap.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="custom.js"></script>
+
 </body>
+
 </html>
+
